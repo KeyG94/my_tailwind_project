7e7293d170a91e5556a903643c2ce0ee7fca8f4a
Glenn Key <glennkey78@live.no>
2020-09-17 18:41:48 +0200

    first commit, added a header, h1, sample text and a photo of a car

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // where does this file come from?
+    plugins: [
+        //require('postcss-loader'),
+        require('postcss-import'),
+        // require('postcss-scss'),
+        require('tailwindcss'),
+        //require('autoprefixer'),
+    ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      textColor: {
+        'primary': 'blue',
+      }
+    },
+  },
+  variants: {},
+  plugins: [
+    require('postcss-scss'),
+  ],
+}
